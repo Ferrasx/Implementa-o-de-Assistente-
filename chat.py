bc5d0caadeee42ef79d72cae8292c99c3866b11d
Gabriel Martins Ferraz <84094936+Ferrasx@users.noreply.github.com>
2024-11-29 17:46:10 -0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,8 +1,3 @@
-!pip install streamlit
-!pip install openai
-!pip install pinecone-client
-!pip install PyPDF2
-!pip install sentence-transformers
 
 import openai
 from pinecone import Pinecone, ServerlessSpec
